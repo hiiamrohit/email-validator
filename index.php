cc7e5b68268cc0649c6a7c66c1946193692ffb52
Rohit Kumar <rohit0kumar@hotmail.com>
2015-04-30 16:09:30 +0530

    update

--- index.php
+++ index.php
@@ -9,9 +9,28 @@
         <link href="css/custom.css" rel="stylesheet">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+        <meta name="viewport" content="width=device-width">
     </head>
+    <style>
+     .bottombar {
+    position: fixed;
+    z-index: 100; 
+    top: 0; 
+    left: 0;
+    width: 100%;
+    background-color: #333333;
+    text-align:center;
+    color:#ffffff;
+  }
+    </style>
     <body>
-
+<div class="bottombar">
+   <div class="left"><span class="badge">Copyright © <?= date('Y'); ?></span></div>
+   
+   <div class="right"><span class="badge">Developed By <a href="http://www.iamrohit.in" target="_blank">Rohit Kumar</a></span></div>
+   </div>
+ 
+   <div style="padding-top:1%">
         <!-- Modal -->
         <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
             <div class="modal-dialog">
@@ -45,7 +64,8 @@
 
         <div id="contextFirst" > 
             <div class="panel panel-primary">
-                <div class="panel-heading"><h3>Enter the Email Id (You can also add multiple email ids using comma)</h3>
+                <div class="panel-heading"><h3>Email id validator to check email id is real or fake, Use comma to add multiple email id's</h3>
+               
                 </div>
                 <div class="panel-body" style="height:40%; text-align:center;" >
                     <form role="form" class="form-inline" id="fdata" >
@@ -53,7 +73,7 @@
                         <div class="form-group" style= "width:79%;">
                             <input type="hidden" name="req" value="blog">
                             <input type="input" style= "width:100%;" class="form-control input-lg" name="email"
-                                   id="email" required="required"  placeholder="Enter email ids (Exp: iamrohitx@gmail.com, hi@iamrohit.in) !!">
+                                   id="email" required="required"  placeholder="Enter email id's (Exp: iamrohitx@gmail.com, hi@iamrohit.in) !!">
 
                         </div>
                        
